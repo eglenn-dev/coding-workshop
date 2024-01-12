0820c10d465056c64d0db77f84f831a962785981
Ethan Glenn <ethan+git@eglenn.dev>
2024-01-11 18:58:56 -0700

    Added the layout for the program

---
+++ get-started.py
@@ -0,0 +1,21 @@
+# ====== WELCOME ======
+
+# Import the needed libraries
+
+# Initialize the screen
+
+# Make a for loop to 
+
+    # Another for loop for drawing line. 
+
+    # Turn around and reset position
+
+
+# Create the first player
+
+# Create up to three more players
+
+
+# Set up a for loop for the actual race
+
+    # Set each player to move at a random interval
